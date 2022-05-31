2b9d986373b57c3177348f471a55e6a050070f3c
Michael Maker <maker.mdm@gmail.com>
2022-05-31 16:40:07 -0600

    Initial encode

---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import { useState } from 'react'
+
+
+const Footer = (props) => {
+  // STATE**************************************
+  // const [show, setShow] = useState(false);
+
+
+  //EVENTHANDLER********************************
+
+
+
+  //**************************************************
+  return (
+
+    <>
+      <header className="top-nav">
+
+        <h4 className="page-title">
+         
+        </h4>
+
+      </header>
+    </>
+
+  )
+}
+export default (Footer)
---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import { useState } from 'react'
+
+
+const Header = (props) => {
+  // STATE**************************************
+  // const [show, setShow] = useState(false);
+
+
+  //EVENTHANDLER********************************
+
+
+
+  //**************************************************
+  return (
+
+    <>
+      <header className="top-nav">
+
+        <h1 className="page-title">
+         W3resource Exercises
+        </h1>
+
+      </header>
+    </>
+
+  )
+}
+export default (Header)
