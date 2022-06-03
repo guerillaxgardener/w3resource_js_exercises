36c264bf3373d9ff6245a4c45220b66af783c0b0
Michael Maker <maker.mdm@gmail.com>
2022-06-03 13:19:56 -0600

    Solve exercise 6, add Button & Overlay

--- src/exercises/Ex6.js
+++ src/exercises/Ex6.js
@@ -1,12 +1,27 @@
 //6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 
 import React from "react";
-import { Card } from "react-bootstrap";
-// import { useState } from 'react'
+import { useState } from 'react';
+import { useRef } from 'react';
+import { Card, Button, Overlay } from "react-bootstrap";
 
 
-const ex6 = (props) => {
 
+const Ex6 = (props) => {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+  let date = new Date();
+  let currentYear = date.getFullYear();
+  let leapTruth = false;
+
+  if (currentYear % 4 === 0 && currentYear % 100 !== 0) {
+    leapTruth = true;
+  } else if (currentYear % 4 === 0 && currentYear % 100 === 0 && currentYear % 400 === 0) {
+    leapTruth = true;
+  } else {
+    leapTruth = false;
+  }
 
 
   return (
@@ -17,9 +32,35 @@ const ex6 = (props) => {
 
 
       <Card.Body>
+        <Button variant="danger" ref={target} onClick={() => setShow(!show)}>
+          is {currentYear} a leap year?
+        </Button>
+
+
         <Card.Text>
-          SOLUTION: <br />
-       
+
+        <Overlay target={target.current} show={show} placement="right">
+          {({ placement, arrowProps, show: _show, popper, ...props }) => (
+            <div
+              {...props}
+              style={{
+                position: 'absolute',
+                backgroundColor: 'rgba(255, 100, 100, 0.85)',
+                padding: '2px 10px',
+                color: 'white',
+                borderRadius: 3,
+                ...props.style,
+              }}
+            >
+              {leapTruth === true
+              ?
+             " YES IT IS!!!"
+              :
+              "Not a leap year."
+              }
+            </div>
+          )}
+        </Overlay>
         </Card.Text>
       </Card.Body>
 
@@ -28,4 +69,4 @@ const ex6 = (props) => {
   )
 }
 
-export default (ex6)
+export default (Ex6)
