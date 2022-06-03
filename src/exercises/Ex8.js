fbbe902fca464403bcca04d1267c6c8736806b8a
Michael Maker <maker.mdm@gmail.com>
2022-06-03 12:48:19 -0600

    Encode Exercise 5-8

---
+++ src/exercises/Ex8.js
@@ -0,0 +1,31 @@
+//8. Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched".
+
+import React from "react";
+import { Card } from "react-bootstrap";
+// import { useState } from 'react'
+
+
+const ex8 = (props) => {
+
+
+
+  return (
+    <>
+
+      <Card.Title>Exercise 8: Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched".
+      </Card.Title>
+
+
+      <Card.Body>
+        <Card.Text>
+          SOLUTION: <br />
+       
+        </Card.Text>
+      </Card.Body>
+
+    </>
+
+  )
+}
+
+export default (ex8)
