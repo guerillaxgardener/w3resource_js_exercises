5dbd1b88b7e0f7c091ef62a022ee4d3a2cddd421
Michael Maker <maker.mdm@gmail.com>
2022-06-06 10:21:04 -0600

    Update exercise to have dynamic triangle lengths

--- src/exercises/Ex4.js
+++ src/exercises/Ex4.js
@@ -1,31 +1,82 @@
 //4. Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
 
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
-// import { useState } from 'react'
+import '../exercise.css';
 
 
-const ex4 = (props) => {
+const Ex4 = (props) => {
+  const [side1, setSide] = useState(0)
+  const [side2, setSide2] = useState(0)
+  const [side3, setSide3] = useState(0)
+  const [area, getArea] = useState(0)
+  const [areaDisplay, handleShow] = useState(false)
+  console.log('side 1 =', side1, 'side 2 =', side2, 'side 3 =', side3)
 
-  let a = 5;
-  let b = 6;
-  let c = 7;
 
-  let s = ((a + b + c) / 2);
+  const handleSide1 = (e) => setSide(e.target.value)
+  const handleSide2 = (e) => setSide2(e.target.value)
+  const handleSide3 = (e) => setSide3(e.target.value)
+  const displayArea = () => handleShow(true)
 
-  let area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let s = ((parseFloat(side1) + parseFloat(side2) + parseFloat(side3)) / 2)
+    let area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)))
+    getArea(area);
+    displayArea();
+  }
 
   return (
     <>
 
-      <Card.Title>Exercise 4: Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
+      <Card.Title className="exercise-title">Exercise 4: Write a JavaScript program to find the area of a triangle when user provides lengths.
       </Card.Title>
 
 
       <Card.Body>
+
+        <form onSubmit={e => { handleSubmit(e) }}>
+          <label>
+            Side 1:
+            <input
+              type="number"
+              name="side1"
+              onInput={handleSide1}
+            />
+          </label>
+
+          <label>
+            Side 2:
+            <input
+              type="number"
+              name="side2"
+              onInput={handleSide2}
+
+            />
+          </label>
+
+          <label>
+            Side 3:
+            <input
+              type="number"
+              name="side3"
+              onInput={handleSide3}
+
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
         <Card.Text>
-          SOLUTION: <br />
-          area of triangle = {area}
+          {areaDisplay
+            &&
+            <>
+              SOLUTION: <br />
+
+              the area of triangle with lengths {side1}, {side2}, and {side3} is {area}`
+            </>
+          }
         </Card.Text>
       </Card.Body>
 
@@ -34,4 +85,4 @@ const ex4 = (props) => {
   )
 }
 
-export default (ex4)
+export default (Ex4)
