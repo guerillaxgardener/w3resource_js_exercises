de5bc77e223cc76c86b6ec64ebdafe00062182cf
Michael Maker <maker.mdm@gmail.com>
2022-06-06 13:03:51 -0600

    Add classes

--- src/exercises/Ex2.js
+++ src/exercises/Ex2.js
@@ -1,37 +1,38 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-// import { useState } from 'react'
+import '../exercise.css';
 
+const Ex2 = (props) => {
 
-const ex2 = (props) => {
-
-let printThePage = () => {
-  window.print();
-}
+  let printThePage = () => {
+    window.print();
+  }
 
   return (
 
     <>
-     
-        <Card.Title>Exercise 2: Write a JavaScript program to print the contents of the current window.</Card.Title>
 
-        <Card.Body>
-          <Card.Subtitle>
+      <Card.Title className="exercise-title">
+        Ex 2: Print contents of the current window.
+      </Card.Title>
 
-          </Card.Subtitle>
+      <Card.Body className="exercise-body">
+        <Card.Subtitle className="exercise-expected-output">
 
-          <Card.Text>
-            SOLUTION: <br />
-            Click the button to print the page!!
-            <Button onClick={printThePage}>
-              Click t0o print!!!
-            </Button>
-          </Card.Text>
-        </Card.Body>
- 
+        </Card.Subtitle>
+
+        <Card.Text className="exercise-solution">
+          SOLUTION: <br />
+          Click button to print the page!
+          <Button onClick={printThePage}>
+            Click to print!!
+          </Button>
+        </Card.Text>
+      </Card.Body>
+
     </>
 
   )
 }
 
-export default (ex2)
+export default (Ex2)
