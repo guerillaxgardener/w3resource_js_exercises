8ea350c0c7e94cdb7331acdad11ba4dddc6c36c2
Michael Maker <maker.mdm@gmail.com>
2022-06-14 14:14:24 -0600

    Encode and complete Ex 10

---
+++ src/exercises/Ex10.js
@@ -0,0 +1,112 @@
+//10. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit. 
+
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import '../exercise.css';
+import { useState } from "react";
+
+const Ex10 = (props) => {
+  const [temp, handleTemp] = useState(null)
+  const [convert, handleBaseUnit] = useState(null)
+  const [finalTemp, convertifyMeCaptain] = useState(null)
+
+  const handleTempTyped = (e) => {
+    handleTemp(parseInt(e.target.value))
+  }
+
+  const conversion = (e) => {
+    handleBaseUnit(e.target.value)
+    convertifyMeCaptain(null)
+  }
+
+  const doConversion = (e) => {
+    (convert === "celsius")
+      ?
+      convertifyMeCaptain((temp * 1.8000) + 32.00)
+      :
+      convertifyMeCaptain((temp - 32.00) / 1.80)
+  }
+
+  return (
+    <>
+      <Card.Title className="exercise-title">
+        Exercise 10: Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+      </Card.Title>
+      <Card.Body className="exercise-body">
+        <form>
+
+          <label>
+            Enter Temp:
+            <input
+              type="integer"
+              name="tempInput"
+              onInput={handleTempTyped}
+            />
+          </label>
+
+
+          <input
+            type="radio"
+            id="celsius"
+            name="temp_unit"
+            value="celsius"
+            onClick={e => { conversion(e) }}
+          />
+
+          <label
+            for="html">
+            Convert &deg;C to &deg;F
+          </label>
+
+          <br />
+
+          <input
+            type="radio"
+            id="farenheit"
+            name="temp_unit"
+            value="fahrenheit"
+            onClick={e => { conversion(e) }}
+          />
+
+          <label for="css">
+            Convert &deg;F to &deg;C
+          </label>
+
+        </form>
+
+      </Card.Body>
+
+      <Card.Text className="exercise-solution">
+        {convert && temp
+          &&
+          <Button onClick={e => { doConversion(e) }}>
+            CONVERT!
+          </Button>}
+
+        {finalTemp
+          &&
+          convert === 'celsius'
+          &&
+          <>
+            <br />
+            final temp is {finalTemp} fahrenheit
+          </>
+        }
+        {finalTemp
+          &&
+          convert === 'fahrenheit'
+          &&
+          <>
+            <br />
+            final temp is {finalTemp} celsius
+          </>
+        }
+
+      </Card.Text>
+
+
+    </>
+  )
+}
+
+export default (Ex10)
