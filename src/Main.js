00d67ef3bdedc484f49e7f31cb42686a96c08ded
Michael Maker <maker.mdm@gmail.com>
2022-06-14 12:30:15 -0600

    Add Ex9

--- src/Main.js
+++ src/Main.js
@@ -8,6 +8,8 @@ import Ex5 from "./exercises/Ex5";
 import Ex6 from "./exercises/Ex6";
 import Ex7 from "./exercises/Ex7";
 import Ex8 from "./exercises/Ex8";
+import Ex9 from "./exercises/Ex9";
+
 import "../src/Main.css";
 // import { useState } from 'react'
 
@@ -49,6 +51,10 @@ const Main = (props) => {
 
         <Card className="exercise-card" style={{ width: '18rem' }}>
           <Ex8 />
+        </Card>
+
+        <Card className="exercise-card" style={{ width: '18rem' }}>
+          <Ex9 />
         </Card>
 
       </Container >
